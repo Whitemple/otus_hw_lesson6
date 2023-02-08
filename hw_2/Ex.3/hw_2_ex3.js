e77b27cd351821028c5d0acc39b6ac1b44e2276f
whitemple <yarikkhromey@mail.ru>
2023-02-08 16:28:12 +0300

    done exercise 3

--- hw_2/Ex.3/hw_2_ex3.js
+++ hw_2/Ex.3/hw_2_ex3.js
@@ -3,3 +3,17 @@
 и квадрата соответственно. Написать программу,
 которая определяет, поместится ли круг в квадрат
 */
+
+function checkTheSquare(r,a){
+    const radius = Math.pow(r,2);
+    const sSquare = Math.pow(a,2);
+    const sCircle = Math.PI*radius;
+    if(sCircle<=sSquare){
+        return 'Inside';
+    }
+    else{
+        return 'Outside';
+    }
+};
+
+module.exports={checkTheSquare};
--- hw_2/Ex.3/hw_2_ex3.test.js
+++ hw_2/Ex.3/hw_2_ex3.test.js
@@ -3,3 +3,17 @@
 и квадрата соответственно. Написать программу,
 которая определяет, поместится ли круг в квадрат
 */
+
+const {checkTheSquare} = require('./hw_2_ex3');
+
+describe('testing checkTheSquare function: ',()=>{
+    test("Should return inside for 2 and 7 ",()=>{
+        expect(checkTheSquare(2,7)).toEqual('Inside');
+    });
+    test("Should return Outside for 5 and 3 ",()=>{
+        expect(checkTheSquare(5,3)).toEqual('Outside');
+    });
+    test("Should return inside for 4 and 9 ",()=>{
+        expect(checkTheSquare(4,9)).toEqual('Inside');
+    });
+})
