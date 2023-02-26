8a519d056babe2a992f98d2a424d0f2a9a285f0e
whitemple <yarikkhromey@mail.ru>
2023-02-26 13:51:37 +0300

    add exercise 9 about Math operations

---
+++ src/hw_9/hw_9_ex1.js
@@ -0,0 +1,21 @@
+const checkTriangle = () => {
+    const arr = prompt("Enter 3 numbers: ");
+    const [a, b, c] = arr.split('');
+    if((a*a)+(b*b)==c*c){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+const aboutCircle = () =>{
+    const r = prompt('Enter radius: ');
+    const p = 2*Math.PI*r
+    const s = Math.PI*(r*r);
+    const result = `Длина окружности ${Math.round(p)} и площадь круга ${Math.round(s)}`
+    return result;
+}
+
+
+
+module.exports={checkTriangle, aboutCircle};
